ffbd21d39be15c0756ea9c1cb0c11f608e77a5a6
maksb <maksbolshy@mail.ru>
2023-10-31 18:33:50 +0300

    djbavlenie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = /* мой список студентов */;
+
+        List<Student> topStudents = students.stream()
+                .filter(student -> "Информатика".equals(student.getSpecialty()))
+                .filter(student -> student.getAverageGrade() > 4.5)
+                .sorted((s1, s2) -> Double.compare(s2.getAverageGrade(), s1.getAverageGrade()))
+                .limit(5)
+                .collect(Collectors.toList());
+
+        topStudents.forEach(student -> System.out.println(student.getName() + ": " + student.getAverageGrade()));
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Student {
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Student {
+    private String name;
+    private List<Double> grades;
+    private String specialty;
+
+    public double getAverageGrade() {
+        return grades.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
+    }
 }
